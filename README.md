a1becdb176aa818860de119d02fa1aca2bf205e1
Xacnio <alperenitec@yandex.com>
2021-10-27 21:46:32 +0300

    cli output flag

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # tweetcapture-go
-.
+## Command-line Usage
+```
+NAME:
+   tweetcapture-go - simple tweet screenshot tool
+
+USAGE:
+   tweetcapture-go [options] [Tweet URL]
+
+COMMANDS:
+   help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --nightmode value, -n value  0-Light / 1-Dim / 2-Lights out (default: 0)
+   --output value, -o value     File path to save screenshot
+   --help, -h                   show help
+   --version, -v                print the version
+```
--- cmd/cmd.go
+++ cmd/cmd.go
@@ -14,7 +14,7 @@ func RunCmd() {
 	app.Name = "tweetcapture-go"
 	app.Usage = "simple tweet screenshot tool"
 	app.UsageText = app.Name + " [options] [Tweet URL]"
-	app.Version = "v1.0.0"
+	app.Version = "v1.0.1"
 
 	registerArgs(app)
 	registerAction(app)
@@ -29,6 +29,7 @@ func registerArgs(app *cli.App) {
 	app.Flags = []cli.Flag{
 		cli.UintFlag{Name: "nightmode, n", Value: 0, Usage: "0-Light / 1-Dim / 2-Lights out"},
 		cli.BoolFlag{Name: "seleniumdebug, sd", Hidden: true},
+		cli.StringFlag{Name: "output, o", Value: "", Usage: "File path to save screenshot"},
 	}
 }
 
@@ -41,6 +42,7 @@ func registerAction(app *cli.App) {
 		tss := tweetcapture.NewTweetScreenshot(url)
 		tss.Opts.NightMode = uint8(c.Uint("nightmode"))
 		tss.Opts.SeleniumDebug = c.Bool("seleniumdebug")
+		tss.SavePath = c.String("output")
 		err := tss.Screenshot()
 		if err != nil {
 			return cli.NewExitError(err, 26)
